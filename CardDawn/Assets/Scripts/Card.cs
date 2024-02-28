60b22a25fac76aa091e9afc786a209c4f1ac76e3
D4R102004 <161531325+D4R102004@users.noreply.github.com>
2024-02-27 18:24:41 -0700

    First Up

---
+++ CardDawn/Assets/Scripts/Card.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+class Card :using UnityEngine;
+
+public class Card : MonoBehaviour {
+	
+}
+{
+	public int CardID;
+	public string Name;
+	public string Type;
+	public int Power;
+}
+
+
+public Card() 
+{
+
+}
+public Card(int cardid, string name, string type, int power)
+{
+	cardid = CardID
+	name = Name
+	type = Type
+	power = Power
+}
